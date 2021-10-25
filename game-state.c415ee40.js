78948925780c7a052681df09b6b3996978f078b3
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-10-25 20:56:22 +0100

    release

--- game-state.c415ee40.js
+++ game-state.c415ee40.js
@@ -269,7 +269,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65366" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63592" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
