e3edd9262feee809bbcaf955242335c0be2a188e
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-10-27 00:03:34 +0100

    code restructure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import 'regenerator-runtime/runtime'
 import { Camera } from './camera';
-import { predict, handlePoseToGameEvents } from './predictions'
+import { predict } from './predictions'
 import { handleMoveToEvent } from './game-state'
+import * as params from './pose-detection-cfg';
+import { getAngleBetween } from './angles';
+import { left, right, up, stop } from './game-state'
 
 
 const startGame = async () => {
@@ -28,6 +31,72 @@ const startGame = async () => {
 
     // ai
     predictPose(camera)
+}
+
+// TODO implement jump up move after 3 stop, up moves 
+let movedUp = false
+const handlePoseToGameEvents = (pose) => {
+    const poseKeypoints = pose.keypoints
+
+    const nose = poseKeypoints[0]
+
+    const leftEye = poseKeypoints[1]
+    const rightEye = poseKeypoints[2]
+
+    const leftShoulder = poseKeypoints[5]
+    const rightShoulder = poseKeypoints[6]
+
+    const leftElbow = poseKeypoints[7]
+    const rightElbow = poseKeypoints[8]
+
+    const leftElbowToSholder = getAngleBetween(leftShoulder, leftElbow)
+    const rightElbowToSholder = getAngleBetween(rightShoulder, rightElbow)
+
+    // arms and elbows
+    const angle = 40
+    const bothArmsUp = (leftElbowToSholder > angle)
+        && (rightElbowToSholder > angle)
+
+    const noseToLeftEyeYdistance = nose.y - leftEye.y
+    const noseToRightEyeYdistance = nose.y - rightEye.y
+
+    // vissibility
+    const scoreThreshold = params.PoseDetectionCfg.modelConfig.scoreThreshold || 0;
+
+    const noseVissible = nose.score > scoreThreshold
+    const lEVissible = leftEye.score > scoreThreshold
+    const REVissible = rightEye.score > scoreThreshold
+
+    const lShoulderVissible = leftShoulder.score > scoreThreshold
+    const rShoulderVissible = rightShoulder.score > scoreThreshold
+    const lElbowVissible = leftElbow.score > scoreThreshold
+    const rElbowVissible = rightElbow.score > scoreThreshold
+
+    const shouldersVisible = lShoulderVissible && rShoulderVissible
+
+    let visibleShoulders = 0
+    if (lElbowVissible) {
+        visibleShoulders += 1
+    }
+    if (rElbowVissible) {
+        visibleShoulders += 1
+    }
+    const shouldersAndElbowsVissible = shouldersVisible && visibleShoulders == 2
+
+    const moveSideActivationDist = 8
+    if (noseVissible && lEVissible
+        && noseToLeftEyeYdistance < moveSideActivationDist) {
+        return left;
+    } else if (noseVissible && REVissible
+        && noseToRightEyeYdistance < moveSideActivationDist) {
+        return right;
+    } else if (shouldersAndElbowsVissible && bothArmsUp) {
+        movedUp = true
+        return up;
+    } else {
+        movedUp = false
+        return stop;
+    }
 }
 
 const predictPose = async (camera) => {
--- src/predictions.js
+++ src/predictions.js
@@ -1,7 +1,5 @@
 import 'regenerator-runtime/runtime'
-import { getAngleBetween } from './angles';
 import * as params from './pose-detection-cfg';
-import { left, right, up, stop } from './game-state'
 import * as tf from '@tensorflow/tfjs-core';
 import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
 import * as poseDetection from '@tensorflow-models/pose-detection';
@@ -26,72 +24,6 @@ const setupTf = async () => {
 
 setupTf()
 
-// TODO implement jump up move after 3 stop, up moves 
-let movedUp = false
-const handlePoseToGameEvents = (pose) => {
-    const poseKeypoints = pose.keypoints
-
-    const nose = poseKeypoints[0]
-
-    const leftEye = poseKeypoints[1]
-    const rightEye = poseKeypoints[2]
-
-    const leftShoulder = poseKeypoints[5]
-    const rightShoulder = poseKeypoints[6]
-
-    const leftElbow = poseKeypoints[7]
-    const rightElbow = poseKeypoints[8]
-
-    const leftElbowToSholder = getAngleBetween(leftShoulder, leftElbow)
-    const rightElbowToSholder = getAngleBetween(rightShoulder, rightElbow)
-
-    // arms and elbows
-    const angle = 40
-    const bothArmsUp = (leftElbowToSholder > angle)
-        && (rightElbowToSholder > angle)
-
-    const noseToLeftEyeYdistance = nose.y - leftEye.y
-    const noseToRightEyeYdistance = nose.y - rightEye.y
-
-    // vissibility
-    const scoreThreshold = params.PoseDetectionCfg.modelConfig.scoreThreshold || 0;
-
-    const noseVissible = nose.score > scoreThreshold
-    const lEVissible = leftEye.score > scoreThreshold
-    const REVissible = rightEye.score > scoreThreshold
-
-    const lShoulderVissible = leftShoulder.score > scoreThreshold
-    const rShoulderVissible = rightShoulder.score > scoreThreshold
-    const lElbowVissible = leftElbow.score > scoreThreshold
-    const rElbowVissible = rightElbow.score > scoreThreshold
-
-    const shouldersVisible = lShoulderVissible && rShoulderVissible
-
-    let visibleShoulders = 0
-    if (lElbowVissible) {
-        visibleShoulders += 1
-    }
-    if (rElbowVissible) {
-        visibleShoulders += 1
-    }
-    const shouldersAndElbowsVissible = shouldersVisible && visibleShoulders == 2
-
-    const moveSideActivationDist = 8
-    if (noseVissible && lEVissible
-        && noseToLeftEyeYdistance < moveSideActivationDist) {
-        return left;
-    } else if (noseVissible && REVissible
-        && noseToRightEyeYdistance < moveSideActivationDist) {
-        return right;
-    } else if (shouldersAndElbowsVissible && bothArmsUp) {
-        movedUp = true
-        return up;
-    } else {
-        movedUp = false
-        return stop;
-    }
-}
-
 // var myWorker = new Worker('worker.js');
 // myWorker.postMessage(JSON.stringify(pose[0]));
 
@@ -108,4 +40,4 @@ const predict = async (imgData) => {
     return poses
 }
 
-export { predict, handlePoseToGameEvents }
+export { predict }
