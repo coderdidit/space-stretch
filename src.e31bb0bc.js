9f34eb31986f2733fda9bc646b1074d2e81c8581
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-11-01 20:00:09 +0000

    fixing bug on measuring when player has won

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -95426,7 +95426,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59500" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55159" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
