1dfa7038e4f97c231b2ab03bdf10f2dc95bbe529
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-11-03 22:35:29 +0000

    cleanup deps

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -67930,7 +67930,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62225" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62413" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
