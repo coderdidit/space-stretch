a69ac6451609c4102cd6b89b7ec31124a7cb03d0
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-11-03 20:02:35 +0000

    add GA

--- game-state.c415ee40.js
+++ game-state.c415ee40.js
@@ -288,7 +288,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56053" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60811" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
